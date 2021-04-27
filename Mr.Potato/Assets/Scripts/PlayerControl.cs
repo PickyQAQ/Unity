7bdd8cfa6c339e03c029853df88ef0bae8763024
PickyQAQ <lipeiqin0727@foxmail.com>
2021-04-27 19:50:04 +0800

    Unity ex 2

--- Mr.Potato/Assets/Scripts/PlayerControl.cs
+++ Mr.Potato/Assets/Scripts/PlayerControl.cs
@@ -8,17 +8,70 @@ public class PlayerControl : MonoBehaviour
 
 
     private Rigidbody2D heroBody;
-    public float force = 100;
+    public float moveForce = 100;
+    public float jumpForce = 100;
+    private float fInput = 0.0f;
+    public float maxSpeed = 5;
+    private bool bFaceRight = true;
+    private bool bGrounded = false;
+    private bool bJump = false;
+    Transform mGroundCheck;
+
+    
+
+
+
+
     void Start()
     {
         heroBody = GetComponent<Rigidbody2D>();
+        mGroundCheck = transform.Find("GroundCheck");  //判断主角是否在Ground上
     }
 
-    // Update is called once per frame
+
+    // Update is called once per frame  !!!!!
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        //if (Input.GetAxis("Horizontal"))
-        heroBody.AddForce(Vector2.right * h * force);
+        fInput = Input.GetAxis("Horizontal");
+        if (fInput < 0 && bFaceRight)  //判断是否满足转身条件
+            flip();
+
+        else if (fInput > 0 && !bFaceRight)  //判断是否满足转身条件
+            flip();
+
+        bGrounded = Physics2D.Linecast(transform.position, mGroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+
+        if (Input.GetButtonDown("Jump") && bGrounded)  //检测到跳跃键和主角在Ground上时，起跳
+            bJump = true;
+    }
+
+
+    
+
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(heroBody.velocity.x) < maxSpeed)  //速度小于最大速度时加力
+            heroBody.AddForce(fInput * moveForce * Vector2.right);
+
+        if (Mathf.Abs(heroBody.velocity.x) > maxSpeed)  //速度大于最大速度时，将速度控制在最大速度
+            heroBody.velocity = new Vector2(Mathf.Sign(heroBody.velocity.x) * maxSpeed, heroBody.velocity.y);
+
+        if(bJump)  //如果起跳，则向上加力
+        {
+            heroBody.AddForce(new Vector2(0f, jumpForce));
+            bJump = false;
+        }
+
+    }
+
+
+    void flip()  //转身
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        bFaceRight = !bFaceRight;
     }
 }
